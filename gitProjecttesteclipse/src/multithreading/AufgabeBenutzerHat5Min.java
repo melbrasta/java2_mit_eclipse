d596de0fcd0f9efa423d7d3b17059aa48cf3f481
Yannick Bauer <melbrasta@users.noreply.github.com>
2022-04-07 15:07:10 +0200

    kommentare zu multithreading

--- gitProjecttesteclipse/src/multithreading/AufgabeBenutzerHat5Min.java
+++ gitProjecttesteclipse/src/multithreading/AufgabeBenutzerHat5Min.java
@@ -15,15 +15,26 @@ public class AufgabeBenutzerHat5Min
 
 	public static void main(String[] args) 
 	{
+		
+/**
+ * 
+ * @author yannickb
+ * hier startet die Usereingabe Methode: "Userinput"
+ */
 		class Userinput extends Thread
-		{	
+		{
+		/**
+		 * überschreiben der Run Methode von Thread
+		 */
 			@Override 
 			public void run() 
 			{
 				Scanner sc = new Scanner(System.in);
 				
-				Timer t1 = new Timer( );
-				
+			/**
+			 * hier startet der Timer, der das Programm bei Ablauf beendet.
+			 */
+				Timer t1 = new Timer( );			
 				t1.schedule(new TimerTask() 
 				{
 
@@ -34,71 +45,60 @@ public class AufgabeBenutzerHat5Min
 						System.out.println("Du hast noch "+  counter + " Sekunden für deine Eingabe." );
 		 				if( this.counter <= 0) 
 		 				{
-	//						this.cancel();
 		 					System.out.println("\nSo wird das nichts du lahme Schnecke.");
 							System.exit(0);
 						}
 						counter--;			
 					}
 				// start, intervall
-				}, 150, 1000);
+				}, 150, 1000);		//Der Timer hat einen Delay von 150ms, damit die Benutzereingabe auf jeden Fall vorher ausgegeben wird
 				
+		/**
+		 * Timer ende
+		 */
+				
+				/**
+				 * 
+				 */
 					System.out.println("Bitte gib deine geheimsten Daten und Bankverbindungen ein:\n");
 					input = sc.nextLine();	
 					if(!input.equals(null))
 					{
+						/**
+						 * wenn eine Eingabe vorliegt, dann soll als erstes der Timer beendet werden...
+						 */
 						t1.cancel();
 						System.out.println("Danke. Ihr Konto wird gerade leer geräumt...\nBitte warten..");
+						
+						/**
+						 * neuer Timer von 2,5 Sekunden bevor die neue Ausgabe kommt
+						 */
+						
 						Timer konto = new Timer();
-						konto.schedule(new TimerTask() {
-
+						konto.schedule(new TimerTask() 
+						{
 							@Override
 							public void run()
 							{
 								System.out.println("Ihr Konto ist jetzt leer. Bitte beehren Sie uns bald wieder.");
 								
 								System.exit(0);
-							}
-							
+							}	
 						}, 2500);
-						
-						
-						
+	
 					}
 					sc.close();
-//				},0,1000);
-				
-			}
-		}
 
-	//	class ThreadTimer extends Thread
-//		{
-	//		@Override 
-		//	public void run() 
-			//{
 				
-				
-//				while(this.isAlive())
-//					System.out.println(this.getId());
-				
-//			}
-//		}
-		
-//		ThreadTimer th1 = new ThreadTimer();
+			}			//Ende vom Überschreiben der Run Methode: "public void run" 
+		}						// Ende Userinput
+
+
 		Userinput uinp = new Userinput();
 		uinp.start();
-	//	th1.start();
-		
-
 		
 		
-
-		
-		
-			
-		
-		
-	}
+	}	//Ende public static void main(String[] args) 
 
 	
 }
